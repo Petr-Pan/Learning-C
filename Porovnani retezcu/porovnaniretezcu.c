f0822ee66e13d5992d685269c2011a94b970fff4
Petr-Pan <panek.pet@seznam.cz>
2017-11-19 19:01:17 +0100

    Add project files.

---
+++ Porovnani retezcu/porovnaniretezcu.c
@@ -0,0 +1,42 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int porovnej(char *t1, char *t2) {
+	/*Pokud je øetìzec t1 > t2, vrátí 1, pokud t1 < t2 vrátí -1, u shody vrátí 0.*/
+	int i = 0;
+	while (t1[i] != "\0") {
+		if (t2[i] == "\0") return 1;
+		else if (t1[i] > t2[i]) return 1;
+		else if (t1[i] < t2[i])	return -1;
+
+		i++;
+	}
+	
+
+}
+
+int porovnej_slovne(char *t1, char *t2, int vysledek_porovnej) {
+	/*Vytiskne slovne vysledek funkce porovnej.*/
+	switch (vysledek_porovnej) {
+	case 1: printf("Såovo %s je vetsi nez slovo %s.", *t1, *t2);
+	case -1: printf("Såovo %s je mensi nez slovo %s.", *t1, *t2);
+	case 0:  printf("Såovo %s se shoduje se slovem %s.", *t1, *t2);
+
+	}
+
+
+}
+
+
+
+int main() {
+	char *t1 = "Kacenka";
+	char *t2 = "Kacanka";
+	
+	porovnej_slovne(*t1, *t2, porovnej(*t1, *t2));
+	
+
+
+	system("pause");
+	return 0;
+}
