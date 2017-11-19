7818ed3a35aaf4b5f51fe711f9438f01e9ef07a5
Petr-Pan <panek.pet@seznam.cz>
2017-11-19 19:30:34 +0100

    Záloha funkce porovnej před dalším pokusem.

--- Porovnani retezcu/porovnaniretezcu.c
+++ Porovnani retezcu/porovnaniretezcu.c
@@ -3,39 +3,45 @@
 
 int porovnej(char *t1, char *t2) {
 	/*Pokud je øetìzec t1 > t2, vrátí 1, pokud t1 < t2 vrátí -1, u shody vrátí 0.*/
-	int i = 0;
-	while (*t1 != "\0") {
-		if (*t2 == "\0") return 1;
+	while (*t1 != '\0') {
+		if (*t2 == '\0') return 1;
 		else if (*t1 > *t2) return 1;
 		else if (*t1 < *t2)	return -1;
-
-		i++;
+		t1++;
+		t2++;
 	}
-	
+	return 0;
 
 }
 
 int porovnej_slovne(char *t1, char *t2, int vysledek_porovnej) {
 	/*Vytiskne slovne vysledek funkce porovnej.*/
 	switch (vysledek_porovnej) {
-	case 1: printf("Såovo %s je vetsi nez slovo %s.", *t1, *t2);
-	case -1: printf("Såovo %s je mensi nez slovo %s.", *t1, *t2);
-	case 0:  printf("Såovo %s se shoduje se slovem %s.", *t1, *t2);
-
+	case 1: 
+		printf("Slovo %s je vetsi nez slovo %s.", *t1, *t2);
+		break;
+	case -1: 
+		printf("Slovo %s je mensi nez slovo %s.", *t1, *t2);
+		break;
+	case 0:  
+		printf("Slovo %s se shoduje se slovem %s.", *t1, *t2);
+		break;
+		
 	}
-
-
+	return 0;
 }
 
 
 
 int main() {
-	char *t1 = "Kacenka";
-	char *t2 = "Kacanka";
+	char str1[] = "Kacenka";
+	char str2[] = "Kacanka";
+	char *st1 = str1;
+	char *st2 = str2;
 	
-	porovnej_slovne(*t1, *t2, porovnej(*t1, *t2));
+	//porovnej_slovne(*t1, *t2, porovnej(*t1, *t2));
+	printf(porovnej(*st1, *st2));
 	
-
 
 	system("pause");
 	return 0;
