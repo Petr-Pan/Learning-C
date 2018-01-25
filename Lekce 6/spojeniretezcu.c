b5c6f14db33e3af9a51c1982facfdef7512fbad9
Petr-Pan <panek.pet@seznam.cz>
2018-01-25 19:10:15 +0100

    Vypíše aspoň druhý řetězec
    Občas crashe. Zatím náhodně.

--- Lekce 6/spojeniretezcu.c
+++ Lekce 6/spojeniretezcu.c
@@ -5,18 +5,15 @@
 char *spojeni(char *t1, char *t2) {
 	int size_t1 = (&*t1)[1] - *t1;
 	int size_t2 = (&*t2)[1] - *t2;
-	const int size_total = size_t1 + size_t2;
-
-	char spojene[size_total]= "\0";
+	char *spojene = (char *)malloc(sizeof(size_t1 + size_t2));
 	int i = 0;
-	while (*t1 != "\0") {
-		char spojene[i] = *t1;
-		i++;
+
+	for (i = 0; i < size_t1; i++) {
+		spojene[i] = *t1;
 		t1++;
 		}
-	while (*t2 != "\0") {
-		char spojene[i] = *t2;
-		i++;
+	for (i = 0; i < size_t2; i++) {
+		spojene[i] = *t2;
 		t2++;
 	}
 	
@@ -29,7 +26,10 @@ int main() {
 	char *t1 = str1;
 	char *t2 = str2;
 	
-	printf(*spojeni(t1, t2));
+	printf(spojeni(t1, t2));
+
+	/*char merged = spojeni(t1, t2);
+	printf(merged);*/
 
 	system("pause");
 	return 0;
