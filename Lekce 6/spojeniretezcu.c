3b6ee210b2794c0aad4bbc8172e59e8a9a5e5b2d
Petr-Pan <panek.pet@seznam.cz>
2018-01-26 01:11:14 +0100

    Funkční, včetně pár řádek kódu navíc pro optional input

--- Lekce 6/spojeniretezcu.c
+++ Lekce 6/spojeniretezcu.c
@@ -1,36 +1,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#pragma warning(disable:4996)
 
 char *spojeni(char *t1, char *t2) {
-	int size_t1 = 8;
-	int size_t2 = 5;
+	int size_t1, size_t2;
+	for (size_t1 = 0; t1[size_t1] != '\0'; size_t1++);
+	for (size_t2 = 0; t2[size_t2] != '\0'; size_t2++);
+
 	char *spojene = (char *)malloc((size_t1 + size_t2) * sizeof(char));
 
 	for (int i = 0; i < size_t1; i++) {
 		spojene[i] = *t1;
 		t1++;
-		}
+	}
 	for (int i = size_t1; i < size_t1 + size_t2 + 1; i++) {
 		spojene[i] = *t2;
 		t2++;
 	}
-	
 	return spojene;
 }
 
 int main() {
 	char str1[] = "Vetinari";
 	char str2[] = "Rufus";
+	
+	/*Pro testy
+	scanf("%s", &str1);
+	fflush(stdin);
+	scanf("%s", &str2);*/
+
+
 	char *t1 = str1;
 	char *t2 = str2;
 	
-	printf("Spojeni slov %s a %s je %s.\n", str1, str2, spojeni(t1, t2));
+	printf("Spojeni slov '%s' a '%s' je '%s'.\n", str1, str2, spojeni(&str1, &str2));
 
-	/*int size = (&*t1)[1] - *t1;
-	printf(size);*/
-	/*char merged = spojeni(t1, t2);
-	printf(merged);*/
 
 	system("pause");
 	return 0;
